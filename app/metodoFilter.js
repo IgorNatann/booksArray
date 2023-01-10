3b9f83aaa1663ff07ccea3bbda8575b95fafc2c7
IgorNatann <igornatan4@gmail.com>
2023-01-10 14:33:57 -0300

    commit refatoração function livrosFiltrados

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -1,11 +1,33 @@
-const botoes = document.querySelectorAll('.btn') 
+const botoes = document.querySelectorAll(".btn");
 
-botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
+botoes.forEach((btn) => btn.addEventListener("click", filtrarLivros));
 
 function filtrarLivros() {
-    const elementoBtn = document.getElementById(this.id)
-    const categoria = elementoBtn.value
+  const elementoBtn = document.getElementById(this.id);
+  const categoria = elementoBtn.value;
 
-    let livrosFiltrados = categoria == 'disponivel' ? livros.filter(livro => livro.quantidade > 0) : livros.filter( livro => livro.categoria == categoria)
-    exibirOsLivrosNaTela(livrosFiltrados);
+  let livrosFiltrados =
+    categoria == "disponivel"
+      ? filtrarPorDisponibilidade()
+      : filtrarPorCategoria(categoria);
+  exibirOsLivrosNaTela(livrosFiltrados);
+  if (categoria == "disponivel") {
+    exibirValorTotalDosLivrosDisponiveisNaTela()
+  }
+}
+
+function filtrarPorCategoria(categoria) {
+    return livros.filter((livro) => livro.categoria == categoria);
+}
+
+function filtrarPorDisponibilidade() {
+    return livros.filter((livro) => livro.quantidade > 0);
+}
+
+function exibirValorTotalDosLivrosDisponiveisNaTela() {
+  elementoComValorTotalDeLivrosDisponiveis.innerHTML = `
+    <div class="livros__disponiveis">
+        <p>Todos os livros disponíveis por R$ <span id="valor">299,00</span></p>
+    </div>
+    `;
 }
